4fe0792ca6ac9f6cc2ae52ba4a43761ccc80becb
Timotije Sabo <57005164+timko7@users.noreply.github.com>
2020-01-21 14:31:10 +0100

    Created model for user entity #1_1.1

--- APM/src/app/lekar/home-page-lekar-component.ts
+++ APM/src/app/lekar/home-page-lekar-component.ts
@@ -1,21 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { Routes, Router } from '@angular/router';
+import { Lekar } from './lekar';
+import { LekarServces } from './lekar.services';
 
 
 
 @Component({
   templateUrl: './home-page-lekar.html',
+  styleUrls: ['home-page-lekar.css']
 
 })
 export class HomePageLekarComponent{
 
-  //nazivAdmina: string = "Pera Peric(hardcore)";
+  lekar: Lekar;
 
 
 
-  constructor(private _router: Router) { }
+  constructor(private _router: Router, private LekarSevice: LekarServces) {
+    this.lekar = new Lekar();
+   }
 
   ngOnInit() {
+    this.LekarSevice.getLekaraKlinike().subscribe({
+      next: lekar=>{
+          this.lekar=lekar;
+          console.log('Lekar', this.lekar);
+          /*if(this.admin.prviPutLogovan == true) {
+            alert("Prvi put ste ulogovani!\nMolimo promenite lozinku!");
+          }*/
+          /*this.klinikaServis.getKlinika(this.admin.idKlinike).subscribe({
+            next: klinika => {
+              this.klinika = klinika;
+            }
+          });*/
+      }
+    });
     
   }
 }
